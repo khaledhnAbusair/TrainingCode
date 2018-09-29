5a8f945ce82168ac2a8b332b7a36c35ae0672653
Khaled Abusair <khaled.hnabusair@gmail.com>
2018-09-29 11:05:08 +0300

    reedit packaging folder

---
+++ src/com/abusair/training/codeforceexample/Watermelon.java
@@ -0,0 +1,17 @@
+package com.abusair.training.codeforceexample;
+
+import java.util.Scanner;
+
+public class Watermelon {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int weight = input.nextInt();
+        if (weight > 2 && weight % 2 == 0)
+            System.out.println("YES");
+        else
+            System.out.println("No");
+    }
+
+}

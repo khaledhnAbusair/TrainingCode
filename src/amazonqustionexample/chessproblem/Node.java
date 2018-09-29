fab3b959280e2debb7e0b0ae0628b26876f8acbc
Khaled Abusair <khaled.hnabusair@gmail.com>
2018-09-29 10:51:26 +0300

    init repository

---
+++ src/amazonqustionexample/chessproblem/Node.java
@@ -0,0 +1,18 @@
+package amazonqustionexample.chessproblem;
+
+class Node {
+    // (x, y) represents chess board coordinates
+    // dist represent its minimum distance from the source
+    int x, y, dist;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Node(int x, int y, int dist) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+}

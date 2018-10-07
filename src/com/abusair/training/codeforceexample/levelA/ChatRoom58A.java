e40ec0df273d5e6be019dd0ee9f3aa771e1723b8
Khaled Abusair <khaled.hnabusair@gmail.com>
2018-10-07 12:19:59 +0300

    solve Tram 116A

---
+++ src/com/abusair/training/codeforceexample/levelA/ChatRoom58A.java
@@ -0,0 +1,16 @@
+package com.abusair.training.codeforceexample.levelA;
+
+import java.util.Scanner;
+
+public class ChatRoom58A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String word = "hello";
+        String[] inputArray = input.split("");
+        String[] result = word.split("");
+        if (input.length() > 0 && input.length() < 101) {
+
+        }
+    }
+}
